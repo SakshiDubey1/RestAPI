ede5166d8d506ce5c8ae6738d7c05c62d323fe49
SakshiDubey <sakshidubey2030@gmail.com>
2020-07-21 17:27:13 +0530

    Created different test cases in JUNIT

--- SampleCode/src/test/java/com/SampleCode1/SampleCode/StringHelperTest.java
+++ SampleCode/src/test/java/com/SampleCode1/SampleCode/StringHelperTest.java
@@ -5,10 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringHelperTest {
+	StringHelper helper= new StringHelper();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testtruncate_AInFirst2Positions() {
+		assertEquals("CD",helper.truncateAInFirst2Positions("AACD"));
+		assertEquals("CD",helper.truncateAInFirst2Positions("ACD"));
 	}
-
+	
+	@Test
+	public void testtruncate_ANoWhere() { 
+		assertEquals("CDEF",helper.truncateAInFirst2Positions("CDEF"));
+	}
+	
+	@Test
+	public void testtruncate_AInLast2Position() {
+		assertEquals("CDAA",helper.truncateAInFirst2Positions("CDAA"));
+		assertEquals("BCDA",helper.truncateAInFirst2Positions("BCDA"));
+	}
+	@Test
+	public void testtruncate_whenEnteredNull() {
+		assertEquals("",helper.truncateAInFirst2Positions(""));
+	}
 }
