da371f82f506c1ed9e3d44feba22d01a8bb6538c
SakshiDubey <sakshidubey2030@gmail.com>
2020-07-21 14:20:07 +0530

    First Commit - before development

---
+++ SampleCode/src/test/java/com/SampleCode1/SampleCode/SampleCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SampleCode1.SampleCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCodeApplicationTests {
+//hello
+	@Test
+	void contextLoads() {
+	}
+
+}
